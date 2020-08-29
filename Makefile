a578d490af457de2265ce7261fb68982a6ca91cb
GRN <you@example.com>
2020-08-29 11:33:16 +0200

    TFT functions split from main to library

--- Makefile
+++ Makefile
@@ -52,7 +52,7 @@ TARGET = main
 
 
 # List C source files here. (C dependencies are automatically generated.)
-SRC = $(TARGET).c
+SRC = $(TARGET).c st7735.c
 
 # List Assembler source files here.
 # Make them always end in a capital .S.  Files ending in a lowercase .s
---
+++ st7735.h
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////
+/* credits to http://www.technoblogy.com/show?L6I*/
+/* original library adapted to our needs		 */
+/* 08.2020 GRN									 */
+///////////////////////////////////////////////////
+
+#ifndef ST7735_H
+#define	ST7735_H
+
+#define F_CPU 8000000UL                 // set the CPU clock
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+#include <stdlib.h>
+
+// function prototypes
+void setup(void); 							//Setup
+void TestChart (void);						//draw testchart
+void PlotText(PGM_P p); 						// Plot text starting at the current plot position
+void PlotChar (char c); 						// Plot an ASCII character with bottom left corner at x,y
+void FillRect (int w, int h);				//draw filled rectangle
+void DrawTo (int x, int y);					// Draw a line to x,y
+void PlotPoint (int x, int y); 				// Plot point at x,y
+void MoveTo (int x, int y); 					// Move current plot position to x,y
+unsigned int Colour (int r, int g, int b);	// define colour
+void ClearDisplay (void); 					// clear display
+void DisplayOn (void); 						// turn display on
+void InitDisplay (void);						// initialise display
+void Command4 (uint8_t c, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4); // Send a command followed by four data bytes
+void Command (uint8_t c);					// Send a command to the display
+void Data (uint8_t d);						// Send a byte to the display
+void glcd_draw_circle(uint8_t x0, uint8_t y0, uint8_t r);	// draw circle at position with defined radius
+#endif
