bfa85322861fc6be3124cb9a1f21ba0ada5adc8e
GRN <you@example.com>
2020-08-29 13:27:53 +0200

    added timer function and button definitions

--- main.c
+++ main.c
@@ -23,36 +23,126 @@
 
 #define LED_OFF PORTC &= ~(1<<PC3)
 #define LED_ON PORTC |= (1<<PC3)
+//Buttons 
+#define T_YELLOW !(PIND & (1<<PD5)) && (entprell == 0)
+#define T_BLUE !(PIND & (1<<PD6)) && (entprell == 0)
+#define T_GREEN !(PIND & (1<<PD2)) && (entprell == 0)
+#define LEFT T_BLUE
+#define RIGHT T_YELLOW
+
+#define RELOAD_ENTPRELL 1 //
 
 // Pins already defined in st7735.c
 extern int const DC;
 extern int const MOSI;
 extern int const SCK;
 extern int const CS;
+// Text scale and plot colours defined in st7735.c
+extern int fore; // White
+extern int back;      // Black
+extern int scale;     // Text scale
 
+uint8_t ballposx;
+uint8_t ballposy;
+uint8_t ms10,ms100,sec,min, entprell;
 
+ISR (TIMER1_COMPA_vect)
+{
+	ms10++;
+	if(entprell != 0)entprell--;
+			
+	if(ms10==10)	//10ms
+	{
+		ms10=0;
+		ms100++;
+	}
+    if(ms100==10)	//100ms
+	{
+		ms100=0;
+		sec++;
+	}
+	if(sec==60)	//Minute
+	{
+		sec=0;
+		min++;
+	}
+}
 int main(void)
 {
 	DDRB |= (1<<DC) | (1<<CS) | (1<<MOSI) |( 1<<SCK); 	// All outputs
 	PORTB = (1<<SCK) | (1<<CS) | (1<<DC);          		// clk, dc, and cs high
 	DDRB |= (1<<PB2);									//lcd Backlight output
 	PORTB |= (1<<CS) | (1<<PB2);                  		// cs high
-		
 	DDRC |= (1<<PC3);									//Reset Output
-		
 	DDRD |= (1<<PD7);									//Reset Output
-	PORTD |= (1<<PD7);									//Reset High
+	PORTD |= (1<<PD7);	
+									//Reset High
+	DDRD &= ~((1<<PD6) | (1<<PD2) | (1<<PD5)); 	//Taster 1-3
+	PORTD |= ((1<<PD6) | (1<<PD2) | (1<<PD5)); 	//PUllups für Taster einschalten
+	
+	//Timer 1 Configuration
+	OCR1A = 0x009C;	//OCR1A = 0x3D08;==1sec
+	
+    TCCR1B |= (1 << WGM12);
+    // Mode 4, CTC on OCR1A
+
+    TIMSK1 |= (1 << OCIE1A);
+    //Set interrupt on compare match
+
+    TCCR1B |= (1 << CS12) | (1 << CS10);
+    // set prescaler to 1024 and start the timer
+
+    sei();// enable interrupts
+    
+    ms10=0;
+    ms100=0;
+    sec=0;
+    min=0;
+    entprell=0;
+    
+	ballposx=80;
+	ballposy=80;
+	
+	
 	
 	BACKLIGHT_ON;
 	LED_ON;
 
 	setup();
 	
-	//TestChart();
+	
+	fore = 0x0F00; // White
+	scale = 2;
+	MoveTo(0,0); 
+	DrawTo(0,125);
+	DrawTo(125,125);
+	DrawTo(125,0);
+	DrawTo(0,0);
 	
 	
-  MoveTo(5,10); PlotText(PSTR("Hallo Welt"));
+	
+	//PlotText(PSTR("Roman"));
 
-  glcd_draw_circle(100, 50, 3);
-  for (;;);
-}
+	  
+	  for (;;)
+	  {
+		if(T_GREEN)//move left
+		{
+			entprell=RELOAD_ENTPRELL;
+			ballposx++;
+		} 
+		if(LEFT)//move left
+		{
+			entprell=RELOAD_ENTPRELL;
+			ballposx--;
+		} 
+		if(RIGHT)//move left
+		{
+			entprell=RELOAD_ENTPRELL;
+			ballposy--;
+		} 
+		  glcd_draw_circle(ballposx, ballposy, 3);
+		  
+		  
+	  }//end of for()
+}//end of main
